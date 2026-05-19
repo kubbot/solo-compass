cf51d1b1b17928720b29a1b25be4134060051298
kubbot <3293172751ysy@gmail.com>
2026-05-19 07:05:37 +0000

    fix: remove broken sources_config exports (file not on main)

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,4 +1,2 @@
 export { db } from "./client.js";
 export * from "./schema/index.js";
-export { SOURCE_WEIGHTS, getSourceWeight } from "./sources_config.js";
-export type { KnownSourceName } from "./sources_config.js";
